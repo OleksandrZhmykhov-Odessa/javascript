1f7898617882fb8c4bb80f3bd4a6c54c739acb67
OleksandrZhmykhov-Odessa <zhmykhovgit@gmail.com>
2025-02-10 22:29:54 +0200

    Update math.js

--- math.js
+++ math.js
@@ -1,22 +1,24 @@
-let max = 1000;
-let min = 100;
-let count = 20; 
-let evenCount = 0; 
-let oddCount = 0;  
+function generateRandomNumbers(count) {
+    let max = 1000;
+    let min = 100;
+    let evenCount = 0; 
+    let oddCount = 0;
 
-for (let i = 0; i < count; i++) {
-    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(randomNum);
+    for (let i = 0; i < count; i++) {
+        let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        console.log(randomNum);
 
-    if (randomNum % 2 === 0) {
-        evenCount++; 
-    } else {
-        oddCount++;  
+        if (randomNum % 2 === 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
     }
+
+    let evenPercentage = (evenCount / count) * 100;
+    let oddPercentage = (oddCount / count) * 100;
+
+    console.log(`Парних чисел: ${evenCount} (${evenPercentage.toFixed(2)}%)`);
+    console.log(`Непарних чисел: ${oddCount} (${oddPercentage.toFixed(2)}%)`);
 }
-
-let evenPercentage = (evenCount / count) * 100; 
-let oddPercentage = (oddCount / count) * 100;   
-
-console.log(`Парних чисел: ${evenCount} (${evenPercentage.toFixed(2)}%)`);
-console.log(`Непарних чисел: ${oddCount} (${oddPercentage.toFixed(2)}%)`);
+generateRandomNumbers(20);
